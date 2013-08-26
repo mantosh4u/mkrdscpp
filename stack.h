bc795fd6a60da16546ccd769759c8536f8ef649a
mantosh4u <mantosh4u@gmail.com>
2013-08-26 19:29:40 +0530

    Create stack.h

---
+++ stack.h
@@ -0,0 +1,22 @@
+#ifndef  STACK_H
+#define  STACK_H
+
+namespace mdatastruct {
+
+	class stack
+	{
+	public:
+		stack();
+		~stack();
+		inline bool   empty(void);
+		inline size_t size(void);
+		inline void   push(int);
+		inline int    pop(void);
+	private:
+		enum	{ASIZE = 100};
+		size_t  top;
+		int		S[ASIZE];
+	};
+};
+
+#endif

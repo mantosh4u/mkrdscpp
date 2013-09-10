c4b36f598d76abf224cde12d576c8f2638f3c7fb
mantosh4u <mantosh4u@gmail.com>
2013-09-10 09:22:17 +0530

    Create common.h

---
+++ taocp/common.h
@@ -0,0 +1,19 @@
+/*
+ * common.h
+ *
+ *  Created on: 07-Sep-2013
+ *      Author: mantosh
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include<cstdio>
+#include<cstring>
+#include<cstdlib>
+#include<cctype>
+
+
+#define SIZE 1024
+
+#endif /* COMMON_H_ */

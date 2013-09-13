674c52ccedc1ebf071ab2d7166faf70549d10a4e
mantosh4u <mantosh4u@gmail.com>
2013-09-13 08:59:50 +0530

    Create common.h

---
+++ taocp/chaptertwo/common.h
@@ -0,0 +1,24 @@
+/*
+ * common.h
+ *
+ *  Created on: 12-Sep-2013
+ *      Author: mantosh
+ */
+
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+
+
+
+
+
+
+
+#endif /* COMMON_H_ */

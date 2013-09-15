ac436015076544e4f2e60fd35000d3e41ffdc085
mantosh4u <mantosh4u@gmail.com>
2013-09-16 00:32:35 +0530

    Update common.h

--- taocp/chaptertwo/common.h
+++ taocp/chaptertwo/common.h
@@ -1,10 +1,3 @@
-/*
- * common.h
- *
- *  Created on: 12-Sep-2013
- *      Author: mantosh
- */
-
 #ifndef COMMON_H_
 #define COMMON_H_
 
@@ -12,13 +5,6 @@
 #include<string.h>
 #include<stdlib.h>
 #include<math.h>
-
-
-
-
-
-
-
 
 
 #endif /* COMMON_H_ */
